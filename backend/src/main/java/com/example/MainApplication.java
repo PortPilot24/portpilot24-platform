9703df7a2c403424d4a37ce22294cee4ad60bd38
henson <hslee7231@gmail.com>
2025-08-08 10:09:06 +0900

    chore: remove sensitive file from history and add to .gitignore

--- backend/src/main/java/com/example/MainApplication.java
+++ backend/src/main/java/com/example/MainApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // .env 로딩
+		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD")); // 시스템 프로퍼티 등록
 		SpringApplication.run(MainApplication.class, args);
 	}
 
