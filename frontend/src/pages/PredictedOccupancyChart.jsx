e3bc07bead444d6e87335371d28ef2b42156aca1
henson <hslee7231@gmail.com>
2025-08-12 10:28:55 +0900

    feat: add Dockerfile and docker-compose.yml for frontend service

--- frontend/src/pages/PredictedOccupancyChart.jsx
+++ frontend/src/pages/PredictedOccupancyChart.jsx
@@ -11,7 +11,7 @@ const PredictedOccupancyChart = ({ predictions }) => {
 
   // 예측값(0~1)을 퍼센트와 시간 포맷으로 가공
   const formatted = predictions.map((value, index) => ({
-    time: `${index * 30}분 후`,  // 또는 실제 시간 계산해도 됨
+    time: `${index * 1}시간 후`,  // 또는 실제 시간 계산해도 됨
     점유율예측: (value * 100).toFixed(2),
   }));
 
